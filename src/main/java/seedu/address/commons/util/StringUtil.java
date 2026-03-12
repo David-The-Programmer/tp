37516ec1660867ae1888bdebe7deaf7728ccac51
Jess Kwek <pikachuuu1436@gmail.com>
2026-03-12 14:59:32 +0800

    Add containsSubstringIgnoreCase to StringUtil

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -39,6 +39,36 @@ public class StringUtil {
     }
 
     /**
+     * Returns true if the {@code sentence} contains words that contains the {@code subString}.
+     * Ignores case, does not require a full word match.
+     * <br>
+     * examples:
+     * 
+     * <pre>
+     *       containsWordIgnoreCase("ABc def", "abc") == true
+     *       containsWordIgnoreCase("ABc def", "DEF") == true
+     *       containsWordIgnoreCase("ABc def", "AB") == true //not a full word match
+     *       containsWordIgnoreCase("ABc def", "AC") == false
+     *       containsWordIgnoreCase("ABc def", "c d") == true
+     * </pre>
+     * 
+     * @param sentence  cannot be null
+     * @param substring cannot be null, cannot be empty
+     */
+    public static boolean containsSubstringIgnoreCase(String sentence, String subString) {
+        requireNonNull(sentence);
+        requireNonNull(subString);
+
+        String preppedWord = subString.trim().toLowerCase();
+        checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
+        checkArgument(preppedWord.split("\\s+").length == 1, "Word parameter should be a single word");
+
+        String preppedSentence = sentence.toLowerCase();
+
+        return preppedSentence.contains(preppedWord);
+    }
+
+    /**
      * Returns a detailed message of the t, including the stack trace.
      */
     public static String getDetails(Throwable t) {
